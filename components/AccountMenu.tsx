106d08f6c3871b148b58841e24f03264ec061ced
thegraniti <granitosmani23@gmail.com>
2023-04-23 04:24:36 +0200

    final

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import { signOut } from 'next-auth/react'
-import useCurrentUser from '@/hooks/useCurrentUser';
+import React from "react";
+import { signOut } from "next-auth/react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps {
-    visible?: boolean;
+  visible?: boolean;
 }
 
-const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
-  
-    if(!visible){
-        return null;
-    }
+const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
 
     const { data: user } = useCurrentUser();
-
   
-    return (
-    <div className='bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex'>
-        <div className='flex flex-col gap-3'>
-            <div className='px-3 group/item flex gap-3 items-center w-full'>
-                <img src="/images/default-blue.png" alt="profilepic" className='w-8 rounded-md' />
-                <p className='text-white text-sm group-hover/item:underline'>{user?.name}</p>
-            </div>
-            <hr className='bg-gray-600 border-0 h-px my-4' />
-            <div onClick={() => signOut()} className='px-3 text-white text-center text-sm hover:underline'>
-                Sign out of Netfilx
-            </div>
-        </div>
-    </div>
-  )
-}
+    if (!visible) {
+    return null;
+  }
 
-export default AccountMenu
+
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex gap-3 items-center w-full">
+          <img
+            src="/images/default-blue.png"
+            alt="profilepic"
+            className="w-8 rounded-md"
+          />
+          <p className="text-white text-sm group-hover/item:underline">
+            {user?.name}
+          </p>
+        </div>
+        <hr className="bg-gray-600 border-0 h-px my-4" />
+        <div
+          onClick={() => signOut()}
+          className="px-3 text-white text-center text-sm hover:underline"
+        >
+          Sign out of Netfilx
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountMenu;
